5877ee2312fa04b60fa2bf92d391d5047bfe7269
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-12-01 21:27:42 -0500

    Update app.run with host, port and debug to true

--- run.py
+++ run.py
@@ -3,4 +3,6 @@ from tag_reader import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host='0.0.0.0',
+            port=8000,
+            debug=True)
