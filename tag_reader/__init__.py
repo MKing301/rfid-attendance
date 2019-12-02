8cbac3b50a8bbf053c8ceade82ff63771c029e69
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-12-02 10:11:32 -0500

    Add sqlalchemy db to app init file

--- tag_reader/__init__.py
+++ tag_reader/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from tag_reader.config import Config
 
+
+# Create db
+db = SQLAlchemy()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    db.init_app(app)
+
     from tag_reader.main.views import main
+    from tag_reader.users.views import users
     app.register_blueprint(main)
+    app.register_blueprint(user)
 
     return app
