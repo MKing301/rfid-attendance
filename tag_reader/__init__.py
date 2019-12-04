8f0b1ae83a3c59bf0bbe1440af44b04bf2ff69a0
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-12-04 15:50:54 -0500

    Add login mgr and users blueprint to init

--- tag_reader/__init__.py
+++ tag_reader/__init__.py
@@ -1,20 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from tag_reader.config import Config
 
 
 # Create db
 db = SQLAlchemy()
 
+# Create instance of login manager
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    login_manager.init_app(app)
 
     from tag_reader.main.views import main
     from tag_reader.users.views import users
     app.register_blueprint(main)
-    app.register_blueprint(user)
+    app.register_blueprint(users)
 
     return app
