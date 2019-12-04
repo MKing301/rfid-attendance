c5ae9d34b18009602a4130ff18bdd9e153113079
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-12-04 15:49:36 -0500

    Add models file

---
+++ tag_reader/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, ForeignKey, Integer, String,  DateTime
+from flask import current_app
+from tag_reader import db, login_manager
+from flask_login import UserMixin
+import datetime
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    userid = db.Column(db.String(50), unique=True, nullable=False)
+    profile_pic = db.Column(db.String(255), nullable=False, default='default.jpg')
+    inserted_date =  db.Column(DateTime, default=datetime.datetime.utcnow)
+
+
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rfidtag = db.Column(db.String(255), nullable=False)
+    inserted_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
