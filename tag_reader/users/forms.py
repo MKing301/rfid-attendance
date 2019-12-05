ecdf583e8399919e1862f3509af64117b0f71937
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-12-04 21:05:28 -0500

    Add login form

--- tag_reader/users/forms.py
+++ tag_reader/users/forms.py
@@ -17,3 +17,22 @@ class RegistrationForm(FlaskForm):
                                     validators=[DataRequired(),
                                     EqualTo('password')])
     submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = Users.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = Users.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(),
+                        Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
