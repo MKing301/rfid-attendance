09d2042bbc067bb217db0d9fa3e85650e2ff0673
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-12-04 21:06:37 -0500

    Add login route to views

--- tag_reader/users/views.py
+++ tag_reader/users/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, Blueprint
 from tag_reader import db
 from tag_reader.models import Users
-from tag_reader.users.forms import (RegistrationForm)
+from tag_reader.users.forms import RegistrationForm, LoginForm
 
 users = Blueprint('users', __name__)
 
@@ -10,3 +10,9 @@ users = Blueprint('users', __name__)
 def register():
     form = RegistrationForm()
     return render_template('register.html', title='Register', form=form)
+
+
+@users.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
