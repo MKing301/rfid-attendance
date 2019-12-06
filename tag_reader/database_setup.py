a6ede751a69eb1999b0628dd2ad035eefcaec617
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-12-06 14:46:07 -0500

    Update imports, comments and uri

--- tag_reader/database_setup.py
+++ tag_reader/database_setup.py
@@ -1,21 +1,26 @@
+import os
+import datetime
 from sqlalchemy import Column, ForeignKey, Integer, String,  DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-import datetime
+
 
 Base = declarative_base()
 
 
 class Users(Base):
-    '''A user of the catalog app program. Users will have
+    '''A user in the RFID app program. Users will have
        the following attributes:
        Attibute(s):
        id - unique id number for each user
        first_name = user's first name
        last_name = user's last name
        email - user's email address
-       password - user's passwoclass Book(Base):
+       password - user's password
+       userid - user's associated rfid tag
+       profile_pic - user's profile picture
+       create_date - date user inserted into table
     '''
 
     __tablename__ = 'users'
@@ -27,24 +32,24 @@ class Users(Base):
     password = Column(String(250), nullable=False)
     userid = Column(String(250), nullable=False)
     profile_pic = Column(String(250))
-    inserted_date =  Column(DateTime, default=datetime.datetime.utcnow)
+    create_date =  Column(DateTime, default=datetime.datetime.utcnow)
 
 
 class Log(Base):
-    '''A category for the books in the catalog app program. Categories
-       will have the following attributes:
+    '''A log of scanned rfid tags with timestamp(utc):
        Attibute(s):
        id - unique id number
-       user_id - id of user that created the category
+       rfidtag - rfid tag assigned to user
+       inserted_date - timestamp of log entry in uct
     '''
     __tablename__ = 'log'
 
     id = Column(Integer, primary_key=True)
     rfidtag = Column(String, ForeignKey('users.userid'))
-    inserted_date =  Column(DateTime, default=datetime.datetime.utcnow)
+    date_time =  Column(DateTime, default=datetime.datetime.utcnow)
     users = relationship(Users)
 
 
-engine = create_engine('postgresql://postgres:postgres@localhost:5432/attendence')
+engine = create_engine(os.environ.get('RFID_URI'))
 
 Base.metadata.create_all(engine)
