3adb80b0a7049c9b0740b5ba0b3dd289150f369b
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-12-08 14:56:32 -0500

    Add flask mail to app

--- tag_reader/__init__.py
+++ tag_reader/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 from tag_reader.config import Config
 
 
@@ -16,6 +17,8 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
+mail = Mail()
+
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
@@ -23,6 +26,7 @@ def create_app(config_class=Config):
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     from tag_reader.main.views import main
     from tag_reader.users.views import users
