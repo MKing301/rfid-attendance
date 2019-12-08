9c0377b23f489c8c4c1956895a49b9f01ee6ecee
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-12-08 14:57:24 -0500

    Add mail attr to config & sqlalchemy track mods

--- tag_reader/config.py
+++ tag_reader/config.py
@@ -4,3 +4,10 @@ import os
 class Config:
    SECRET_KEY = os.environ.get('SECRET_KEY_LOCAL')
    SQLALCHEMY_DATABASE_URI = os.environ.get('RFID_URI')
+   SQLALCHEMY_TRACK_MODIFICATIONS = False
+   MAIL_SERVER = 'smtp.googlemail.com'
+   MAIL_PORT = 587
+   MAIL_USE_TLS = True
+   MAIL_USE_SSL = False
+   MAIL_USERNAME =  os.environ.get('MAIL_USERNAME')
+   MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
