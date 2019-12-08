b1b8b08cdeb964d4990f23b5ddae6cd835e65bf2
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-12-07 20:47:34 -0500

    Update table names and columns

--- tag_reader/database_setup.py
+++ tag_reader/database_setup.py
@@ -9,7 +9,7 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-class Users(Base):
+class People(Base):
     '''A user in the RFID app program. Users will have
        the following attributes:
        Attibute(s):
@@ -23,32 +23,32 @@ class Users(Base):
        create_date - date user inserted into table
     '''
 
-    __tablename__ = 'users'
+    __tablename__ = 'people'
 
     id = Column(Integer, primary_key=True)
     first_name = Column(String(250), nullable=False)
     last_name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     password = Column(String(250), nullable=False)
-    userid = Column(String(250), nullable=False)
-    profile_pic = Column(String(250))
-    create_date =  Column(DateTime, default=datetime.datetime.utcnow)
+    userid = Column(String(250), nullable=True, default=None)
+    profile_pic = Column(String(250), nullable=False, default='default.png')
+    created_date =  Column(DateTime, default=datetime.datetime.utcnow)
 
 
-class Log(Base):
+class Logs(Base):
     '''A log of scanned rfid tags with timestamp(utc):
        Attibute(s):
        id - unique id number
        rfidtag - rfid tag assigned to user
        inserted_date - timestamp of log entry in uct
     '''
-    __tablename__ = 'log'
+    __tablename__ = 'logs'
 
     id = Column(Integer, primary_key=True)
-    rfidtag = Column(String, ForeignKey('users.userid'))
-    date_time =  Column(DateTime, default=datetime.datetime.utcnow)
-    users = relationship(Users)
-
+    rfidtag = Column(String(250), nullable=True, default=None)
+    inserted_date =  Column(DateTime, default=datetime.datetime.utcnow)
+    user_id = Column(Integer, ForeignKey('people.id'))
+    people = relationship(People)
 
 engine = create_engine(os.environ.get('RFID_URI'))
 
