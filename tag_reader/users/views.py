efbcd8c82b2d4149dde222cd53e7b721c82f1401
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-12-07 20:51:00 -0500

    Add user auth, routes, form validation and imports

--- tag_reader/users/views.py
+++ tag_reader/users/views.py
@@ -1,6 +1,7 @@
-from flask import render_template, Blueprint
-from tag_reader import db
-from tag_reader.models import Users
+from flask import render_template, url_for, flash, redirect, request, Blueprint
+from flask_login import login_user, current_user, logout_user, login_required
+from tag_reader import db, bcrypt
+from tag_reader.models import People
 from tag_reader.users.forms import RegistrationForm, LoginForm
 
 users = Blueprint('users', __name__)
@@ -8,11 +9,47 @@ users = Blueprint('users', __name__)
 
 @users.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        redirect(url_for('main.home'))
     form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = People(first_name=form.first_name.data,
+                     last_name=form.last_name.data,
+                     email=form.email.data,
+                     password=hashed_password
+                     )
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created! You are now able to log in.', 'success')
+        return redirect(url_for('users.login'))
     return render_template('register.html', title='Register', form=form)
 
 
 @users.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        redirect(url_for('main.home'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = People.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash('Signed in!', 'success')
+            return redirect(url_for('users.dashboard'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@users.route("/dashboard", methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html', title='Dashboard')
+
+
+@users.route("/logout")
+def logout():
+    logout_user()
+    flash('Signed out!', 'success')
+    return redirect(url_for('main.home'))
