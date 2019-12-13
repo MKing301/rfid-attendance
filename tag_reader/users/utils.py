2cef969d694d58f5343f573c91d0ee86e9163217
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-12-13 00:02:39 -0500

    Add function to save pic to user's utililty file

--- tag_reader/users/utils.py
+++ tag_reader/users/utils.py
@@ -1,4 +1,6 @@
 import os
+import secrets
+from PIL import Image
 from flask import url_for, current_app
 from flask_mail import Message
 from tag_reader import mail
@@ -15,3 +17,15 @@ def send_reset_email(user):
 If you did not make this request, simply ignore this email and no change will be made.
 '''.format({url_for('users.reset_token', token=token, _external=True)})
     mail.send(msg)
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
